347ec6d17c85bc3c82c0655e381def45fc1f731d
Kevin Oh <aflashyrhetoric@gmail.com>
2021-02-06 02:11:53 -0500

    Implement auth0 - not a fan yet

--- pages/appledore/index.tsx
+++ pages/appledore/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react"
+import { Auth0Provider } from "@auth0/auth0-react"
 import moment from "moment"
 import Link from "next/link"
 import {
@@ -122,226 +123,239 @@ const Appledore: React.FC = (props: any) => {
 
   return (
     <Page>
-      <>
-        <Modal
-          danger
-          open={deleteModalOpen}
-          modalHeading="Confirm deletion of person and all associated data?"
-          primaryButtonText="Delete"
-          secondaryButtonText="No"
-          onBlur={() => setDeleteModalOpen(false)}
-          onRequestClose={() => setDeleteModalOpen(false)}
-          onRequestSubmit={() => deletePerson(formState.id)}
-        />
+      <Auth0Provider
+        domain="lantern-go.us.auth0.com"
+        clientId="lnRb0HqUtdHjZmb7Zxx5ZmdlZvOvSrkD"
+        redirectUri={"http://localhost:3000/appledore"}
+      >
+        <div>
+          <Modal
+            danger
+            open={deleteModalOpen}
+            modalHeading="Confirm deletion of person and all associated data?"
+            primaryButtonText="Delete"
+            secondaryButtonText="No"
+            onBlur={() => setDeleteModalOpen(false)}
+            onRequestClose={() => setDeleteModalOpen(false)}
+            onRequestSubmit={() => deletePerson(formState.id)}
+          />
 
-        <Modal
-          open={personModalIsOpen}
-          modalHeading={
-            editingMode === EditingState.CREATE
-              ? "Add dossier"
-              : "Update dossier"
-          }
-          primaryButtonText={
-            editingMode === EditingState.CREATE ? "Save" : "Update"
-          }
-          secondaryButtonText="Cancel"
-          selectorPrimaryFocus="#first-name"
-          onRequestSubmit={() => {
-            setLoading(true)
-            if (editingMode === EditingState.CREATE) {
-              createPerson()
+          <Modal
+            open={personModalIsOpen}
+            modalHeading={
+              editingMode === EditingState.CREATE
+                ? "Add dossier"
+                : "Update dossier"
             }
-            if (editingMode === EditingState.UPDATE) {
-              updatePerson(formState.id)
+            primaryButtonText={
+              editingMode === EditingState.CREATE ? "Save" : "Update"
             }
-          }}
-          onRequestClose={() => {
-            resetForm()
-            setPersonModalOpen(false)
-          }}
-          onBlur={() => {
-            resetForm()
-            setPersonModalOpen(false)
-          }}
-        >
-          {/* Loading spinner for table */}
-          <Loading active={loading} />
+            secondaryButtonText="Cancel"
+            selectorPrimaryFocus="#first-name"
+            onRequestSubmit={() => {
+              setLoading(true)
+              if (editingMode === EditingState.CREATE) {
+                createPerson()
+              }
+              if (editingMode === EditingState.UPDATE) {
+                updatePerson(formState.id)
+              }
+            }}
+            onRequestClose={() => {
+              resetForm()
+              setPersonModalOpen(false)
+            }}
+            onBlur={() => {
+              resetForm()
+              setPersonModalOpen(false)
+            }}
+          >
+            {/* Loading spinner for table */}
+            <Loading active={loading} />
 
-          <Form className="some-class" onSubmit={() => {}}>
-            <FormGroup legendText="Basic Information">
-              <TextInput
-                id="first-name"
-                name="first_name"
-                value={(formState && formState.first_name) || ""}
-                placeholder="Thomas"
-                invalid={invalidFields.includes("first_name")}
-                invalidText="A valid value is required"
-                labelText="First name"
-                onChange={e =>
-                  handleTextInputChange(e, "first_name", [
-                    Validations.IsRequired,
-                  ])
-                }
-              />
-              <div style={{ marginBottom: "10px" }} />
-              <TextInput
-                id="last-name"
-                name="last_name"
-                value={(formState && formState.last_name) || ""}
-                placeholder="Shelby"
-                invalid={invalidFields.includes("last_name")}
-                invalidText="A valid value is required"
-                labelText="Last name"
-                onChange={e => handleTextInputChange(e, "last_name", [])}
-              />
-              <div style={{ marginBottom: "10px" }} />
-              <TextInput
-                id="career"
-                name="career"
-                value={(formState && formState.career) || ""}
-                placeholder="Engineer"
-                invalid={invalidFields.includes("career")}
-                invalidText="A valid value is required"
-                labelText="Career"
-                onChange={e => handleTextInputChange(e, "career", [])}
-              />
-              <div style={{ marginBottom: "10px" }} />
-              <TextInput
-                id="mobile"
-                name="mobile"
-                value={(formState && formState.mobile) || ""}
-                placeholder="555-1242"
-                invalid={invalidFields.includes("mobile")}
-                invalidText="A valid value is required"
-                labelText="Mobile"
-                onChange={e => handlePhoneNumberInputChange(e, "mobile")}
-              />
-              <div style={{ marginBottom: "10px" }} />
-              <TextInput
-                id="email"
-                name="email"
-                value={(formState && formState.email) || ""}
-                placeholder="Enter email"
-                invalid={invalidFields.includes("email")}
-                invalidText="A valid value is required"
-                labelText="Email"
-                onChange={e =>
-                  handleTextInputChange(e, "email", [Validations.IsRequired])
-                }
-              />
-              <div style={{ marginBottom: "10px" }} />
-              <TextInput
-                id="address"
-                name="address"
-                value={(formState && formState.address) || ""}
-                placeholder="Enter address"
-                invalid={invalidFields.includes("address")}
-                invalidText="A valid value is required"
-                labelText="Address"
-                onChange={e => handleTextInputChange(e, "address", [])}
-              />
-              <div style={{ marginBottom: "10px" }} />
-              <DatePicker
-                id="date-picker"
-                datePickerType="single"
-                dateFormat="Y-m-d"
-                style={{ marginBottom: "10px" }}
-                value={
-                  formState &&
-                  formState.dob &&
-                  moment.utc(formState.dob).format("YYYY-MM-DD")
-                }
-                onChange={event => {
-                  setFormState({
-                    ...formState,
-                    dob: moment(event[0]).format("YYYY-MM-DD"),
-                  })
-                }}
-              >
-                <DatePickerInput
-                  id="date-picker-input-id"
-                  invalid={invalidFields.includes("dob")}
-                  iconDescription="Select a dob"
-                  labelText="DOB"
-                  placeholder="yyyy-mm-dd"
-                  type="text"
+            <Form className="some-class" onSubmit={() => {}}>
+              <FormGroup legendText="Basic Information">
+                <TextInput
+                  id="first-name"
+                  name="first_name"
+                  value={(formState && formState.first_name) || ""}
+                  placeholder="Thomas"
+                  invalid={invalidFields.includes("first_name")}
+                  invalidText="A valid value is required"
+                  labelText="First name"
+                  onChange={e =>
+                    handleTextInputChange(e, "first_name", [
+                      Validations.IsRequired,
+                    ])
+                  }
                 />
-              </DatePicker>
-            </FormGroup>
-          </Form>
-        </Modal>
+                <div style={{ marginBottom: "10px" }} />
+                <TextInput
+                  id="last-name"
+                  name="last_name"
+                  value={(formState && formState.last_name) || ""}
+                  placeholder="Shelby"
+                  invalid={invalidFields.includes("last_name")}
+                  invalidText="A valid value is required"
+                  labelText="Last name"
+                  onChange={e => handleTextInputChange(e, "last_name", [])}
+                />
+                <div style={{ marginBottom: "10px" }} />
+                <TextInput
+                  id="career"
+                  name="career"
+                  value={(formState && formState.career) || ""}
+                  placeholder="Engineer"
+                  invalid={invalidFields.includes("career")}
+                  invalidText="A valid value is required"
+                  labelText="Career"
+                  onChange={e => handleTextInputChange(e, "career", [])}
+                />
+                <div style={{ marginBottom: "10px" }} />
+                <TextInput
+                  id="mobile"
+                  name="mobile"
+                  value={(formState && formState.mobile) || ""}
+                  placeholder="555-1242"
+                  invalid={invalidFields.includes("mobile")}
+                  invalidText="A valid value is required"
+                  labelText="Mobile"
+                  onChange={e => handlePhoneNumberInputChange(e, "mobile")}
+                />
+                <div style={{ marginBottom: "10px" }} />
+                <TextInput
+                  id="email"
+                  name="email"
+                  value={(formState && formState.email) || ""}
+                  placeholder="Enter email"
+                  invalid={invalidFields.includes("email")}
+                  invalidText="A valid value is required"
+                  labelText="Email"
+                  onChange={e =>
+                    handleTextInputChange(e, "email", [Validations.IsRequired])
+                  }
+                />
+                <div style={{ marginBottom: "10px" }} />
+                <TextInput
+                  id="address"
+                  name="address"
+                  value={(formState && formState.address) || ""}
+                  placeholder="Enter address"
+                  invalid={invalidFields.includes("address")}
+                  invalidText="A valid value is required"
+                  labelText="Address"
+                  onChange={e => handleTextInputChange(e, "address", [])}
+                />
+                <div style={{ marginBottom: "10px" }} />
+                <DatePicker
+                  id="date-picker"
+                  datePickerType="single"
+                  dateFormat="Y-m-d"
+                  style={{ marginBottom: "10px" }}
+                  value={
+                    formState &&
+                    formState.dob &&
+                    moment.utc(formState.dob).format("YYYY-MM-DD")
+                  }
+                  onChange={event => {
+                    setFormState({
+                      ...formState,
+                      dob: moment(event[0]).format("YYYY-MM-DD"),
+                    })
+                  }}
+                >
+                  <DatePickerInput
+                    id="date-picker-input-id"
+                    invalid={invalidFields.includes("dob")}
+                    iconDescription="Select a dob"
+                    labelText="DOB"
+                    placeholder="yyyy-mm-dd"
+                    type="text"
+                  />
+                </DatePicker>
+              </FormGroup>
+            </Form>
+          </Modal>
 
-        <LTable
-          title="dossiers"
-          openModal={() => setPersonModalOpen(true)}
-          openDeleteModal={() => setDeleteModalOpen(true)}
-          updateFormState={data => setFormState(data)}
-          updateEditingMode={setEditingMode}
-          onBatchDelete={(selectedRawRowsData: any[]) => {
-            setLoading(true)
+          <LTable
+            title="dossiers"
+            openModal={() => setPersonModalOpen(true)}
+            openDeleteModal={() => setDeleteModalOpen(true)}
+            updateFormState={data => setFormState(data)}
+            updateEditingMode={setEditingMode}
+            onBatchDelete={(selectedRawRowsData: any[]) => {
+              setLoading(true)
 
-            const promises = selectedRawRowsData.map(data =>
-              deletePersonRequest(data),
-            )
+              const promises = selectedRawRowsData.map(data =>
+                deletePersonRequest(data),
+              )
 
-            Promise.all(promises).then(() => {
-              setLoading(false)
+              Promise.all(promises).then(() => {
+                setLoading(false)
 
-              window.location.reload()
-            })
-          }}
-          rawRowData={people}
-          rowData={people.map(p => ({
-            ...p,
-            id: `${p.id}`,
-            dob: p.dob !== null ? moment.utc(p.dob).format("YYYY-MM-DD") : "-",
-            link: (
-              <Link href={`/appledore/${p.id}`}>
-                <Launch20 className={styles.icon} />
-              </Link>
-            ),
-          }))}
-          headerData={[
-            {
-              header: "ID",
-              key: "id",
-            },
-            {
-              header: "",
-              key: "link",
-            },
-            {
-              header: "Last",
-              key: "last_name",
-            },
-            {
-              header: "First",
-              key: "first_name",
-            },
-            {
-              header: "Career",
-              key: "career",
-            },
-            {
-              header: "Mobile",
-              key: "mobile",
-            },
-            {
-              header: "Email",
-              key: "email",
-            },
-            {
-              header: "Address",
-              key: "address",
-            },
-            {
-              header: "DOB",
-              key: "dob",
-            },
-          ]}
-          disableBatchEdit
-        />
-      </>
+                window.location.reload()
+              })
+            }}
+            rawRowData={people && people.length > 0 ? people : []}
+            rowData={
+              people && people.length > 0
+                ? people.map(p => ({
+                    ...p,
+                    id: `${p.id}`,
+                    dob:
+                      p.dob !== null
+                        ? moment.utc(p.dob).format("YYYY-MM-DD")
+                        : "-",
+                    link: (
+                      <Link href={`/appledore/${p.id}`}>
+                        <Launch20 className={styles.icon} />
+                      </Link>
+                    ),
+                  }))
+                : []
+            }
+            headerData={[
+              {
+                header: "ID",
+                key: "id",
+              },
+              {
+                header: "",
+                key: "link",
+              },
+              {
+                header: "Last",
+                key: "last_name",
+              },
+              {
+                header: "First",
+                key: "first_name",
+              },
+              {
+                header: "Career",
+                key: "career",
+              },
+              {
+                header: "Mobile",
+                key: "mobile",
+              },
+              {
+                header: "Email",
+                key: "email",
+              },
+              {
+                header: "Address",
+                key: "address",
+              },
+              {
+                header: "DOB",
+                key: "dob",
+              },
+            ]}
+            disableBatchEdit
+          />
+        </div>
+      </Auth0Provider>
     </Page>
   )
 }
